c15d94f40f4ff52d77ee3b9f6d6f3ba69959af32
Pavel Mezencev <mezencev24@mail.ru>
2023-06-22 13:31:25 +0300

    2.10.2 Add jest and first tests

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,43 @@
 import { classNames } from './classNames';
 
 describe('classNames', () => {
-  test('correct', () => {
-    expect(classNames('sometest')).toBe('sometest');
+  test('with onlu first param', () => {
+    const expected = 'sometest';
+    expect(classNames('sometest')).toBe(expected);
+  });
+
+  test('with additional classes', () => {
+    const expected = 'sometest class1 class2';
+    expect(classNames('sometest', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods', () => {
+    const expected = 'sometest class1 class2 clicked hovered';
+    expect(classNames(
+      'sometest',
+      { clicked: true, hovered: true },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
+  });
+
+  test('with mods falls', () => {
+    const expected = 'sometest class1 class2 hovered';
+    expect(classNames(
+      'sometest',
+      { clicked: false, hovered: true },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
+  });
+
+  test('with mods undefined', () => {
+    const expected = 'sometest class1 class2 clicked';
+    expect(classNames(
+      'sometest',
+      { clicked: true, hovered: undefined },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
   });
 });
