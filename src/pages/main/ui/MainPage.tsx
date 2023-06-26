e00123c6f0f225ff33c6939c0ce99b577e560fcd
Pavel Mezencev <mezencev24@mail.ru>
2023-06-26 19:12:48 +0300

    27.3 delete big button from main page

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,13 +1,9 @@
-import { BugButton } from 'app/providers/ErrorBoundary/ui/BugButton';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
   return (
-    <>
-      {/* <BugButton /> */}
-      <h1>{t('Главная страница')}</h1>
-    </>
+    <h1>{t('Главная страница')}</h1>
   );
 };
 export default MainPage;
